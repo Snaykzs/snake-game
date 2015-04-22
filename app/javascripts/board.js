3183a8b26220b1b65f177f3539ceee2a2af033d9
Alex Revell & Chhingchhing HEM <pair+alexrevell.chhingchhing@devbootcamp.com>
2015-04-23 11:47:14 +1200

    board create logic

--- app/javascripts/board.js
+++ app/javascripts/board.js
@@ -1,10 +1,18 @@
-Board has
+// Board has
 
-size
-display
-borders
+// size
+// display
+// borders
 
 $(document).ready(function() {
-
+  generateBoard(20);
 
 });
+
+function generateBoard(size) {
+  for(var i=0; i <size; i++) {
+    for(var j=0; j<size; j++) {
+      $('.main-board').append("<div class='cell'></div>")
+    }
+  }
+}
