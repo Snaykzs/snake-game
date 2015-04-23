fa45494e822fc553dc1bab6261083ffef1d64862
Gregory Kan <kan.gregory@gmail.com>
2015-04-24 11:01:08 +1200

    refactored for MVC

--- app/javascripts/game.js
+++ app/javascripts/game.js
@@ -9,10 +9,10 @@ function Game ()
 {
 	this.snake = new Snake();
 	this.lose = false;
-	this.apple = new Apple();
 	this.board = new Board(50);
 	this.board.generateBoard();
-	this.apple.generateApple();
+	this.apple = new Apple(this.board.size);
+	this.generateApple();
 }
 
 Game.prototype.Turn = function () 
@@ -20,12 +20,12 @@ Game.prototype.Turn = function ()
 	var lastRowCoord = this.snake.snakeArray[0][0];
 	var lastColCoord = this.snake.snakeArray[0][1];
 	this.board.renderSnakeCoords(this.snake.snakeArray);
-	this.snake.GetKeyStroke();
+	this.GetKeyStroke();
 	this.snake.Move(this.apple);
-	if (this.snake.eatenApple)
+	if (this.snake.eatenApple === true)
 	{
 		this.board.disActivateApple(this.apple.coordinates[0],this.apple.coordinates[1]);
-		this.apple.generateApple();
+		this.generateApple();
 		this.snake.eatenApple = false;
 	}
 	this.board.disActivateCell(lastRowCoord, lastColCoord);
@@ -33,11 +33,46 @@ Game.prototype.Turn = function ()
 	this.LoseBySelfTouch();
 }
 
+Game.prototype.GetKeyStroke = function ()
+{
+	var thisSnake = this.snake;
+		$(document).keydown(function (e) 
+		{
+			switch(e.which)
+			{
+				case 38:
+				thisSnake.direction = 1;
+				break;
+				case 39:
+				thisSnake.direction = 2;
+				break;
+				case 40:
+				thisSnake.direction = 3;
+				break;
+				case 37:
+				thisSnake.direction = 4;
+				break;
+			}
+
+		});
+}
+
+Game.prototype.generateApple = function()
+{
+	var thisBoard = this.board;
+    this.apple = new Apple(this.board.size);
+    if (thisBoard.isSnake(this.apple.row, this.apple.column))
+		{ 
+		    this.generateApple(); // this is cool
+		};
+    this.board.activateApple(this.apple.row, this.apple.column); 
+};
+
 Game.prototype.CheckLose = function () 
 {
 	var snakeHeadRow = this.snake.snakeArray[this.snake.snakeArray.length-1][0];
 	var snakeHeadCol = this.snake.snakeArray[this.snake.snakeArray.length-1][1];
-	if (snakeHeadRow < 0 || snakeHeadRow > 49 || snakeHeadCol < 0 || snakeHeadCol > 49)
+	if (snakeHeadRow < 0 || snakeHeadRow > this.board.size - 1 || snakeHeadCol < 0 || snakeHeadCol > this.board.size -1)
 	{
 		this.lose = true;
 		alert("you lose!");
