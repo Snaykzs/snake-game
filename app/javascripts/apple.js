e6b25114fe9f77d2f2bd3c9c6ab89f9ad825317b
Alex Revell & Chhingchhing HEM <pair+alexrevell.chhingchhing@devbootcamp.com>
2015-04-23 16:40:01 +1200

    create random apple

--- app/javascripts/apple.js
+++ app/javascripts/apple.js
@@ -3,3 +3,39 @@
 // size
 // random location
 // display
+// require ''
+
+function Apple(){
+  Board.call(this);
+};
+Apple.prototype = Object.create(Board.prototype);
+
+/*Apple.prototype.generateApple = function(){
+
+};*/
+
+Apple.prototype.generateApple = function(){
+  var apple = new Apple();
+  appleRow = Math.floor(Math.random() * apple.size);
+  appleColumn = Math.floor(Math.random() * apple.size);
+  // appleRow = 2;
+  // appleColumn = 3
+  // console.log(apple);
+  console.log(appleRow);
+  console.log(appleColumn);
+  apple.activateApple(appleRow, appleColumn);
+};
+
+// method to generate apple on board
+// method to say apple has been eaten (by snake) (call generate method)
+//
+
+// Apple.prototype.getAppleIcon = function(){
+
+// };
+
+$(document).ready(function(){
+  var apple = new Apple();
+  console.log(apple.test());
+  apple.generateApple();
+})
